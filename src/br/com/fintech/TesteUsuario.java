c6a961da4a403d8472ff18d2dcd5dd8275db20d1
matucaoliveira <matheus.mh1209@gmail.com>
2024-04-23 21:34:13 -0300

    Implementação Usuário

---
+++ src/br/com/fintech/TesteUsuario.java
@@ -0,0 +1,66 @@
+package br.com.fintech;
+
+import br.com.fintech.dao.UsuarioDAO;
+import br.com.fintech.dao.UsuarioDAOImpl;
+import br.com.fintech.entities.Genero;
+import br.com.fintech.entities.Usuario;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public class TesteUsuario {
+	public static void main(String[] args) {
+        loginUsuario();
+        deletarUsuario();
+        getAllUsuarios();
+	}
+	
+	public static void loginUsuario() {
+        try {
+            UsuarioDAO UsuarioDAO = new UsuarioDAOImpl();
+
+            Usuario usuario = new Usuario();
+            usuario.setId(10);
+            usuario.setNomeUsuario("Beatriz");                   
+            usuario.setDtNascimento(LocalDate.of(2001, 12,8));
+            Genero genero = new Genero();
+            genero.setId(2);
+            usuario.setGenero(genero);
+            usuario.setEmail("bea@gmail.com");
+            usuario.setSenha("1234");
+            UsuarioDAO.insert(usuario);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+	}
+		
+	
+	public static void deletarUsuario() {
+        try {
+            int idEdit = 10;
+            UsuarioDAO UsuarioDAO = new UsuarioDAOImpl();
+
+            UsuarioDAO.delete(idEdit);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+	
+	public static void getAllUsuarios() {
+        try {
+            UsuarioDAO usuarioDAO = new UsuarioDAOImpl();
+
+            List<Usuario>  listUsuario = usuarioDAO.getAll();
+
+            for (Usuario cat : listUsuario) {
+                System.out.println(cat.toString());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
---
+++ src/br/com/fintech/dao/UsuarioDAOImpl.java
@@ -0,0 +1,129 @@
+package br.com.fintech.dao;
+import br.com.fintech.connection.FintechDB;
+import br.com.fintech.entities.Categoria;
+import br.com.fintech.entities.Genero;
+import br.com.fintech.entities.Usuario;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class UsuarioDAOImpl implements UsuarioDAO {
+
+    private static final Usuario Usuario = null;
+	private Connection connection;
+    PreparedStatement preparedStatement = null;
+    Statement statement = null;
+
+    String sql;
+    
+    
+    @Override
+    public void insert(Usuario Usuario) throws SQLException {
+        sql = "INSERT INTO T_USUARIO VALUES (?,?,?)";
+        try {
+            connection = FintechDB.getConnectionDB();
+            preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setInt(1, Usuario.getId());
+            preparedStatement.setString(2, Usuario.getNomeUsuario());
+            Date date = Date.valueOf(Usuario.getDtNascimento());
+            preparedStatement.setDate(3, date);
+            preparedStatement.setInt(4, Usuario.getGenero().getId());
+            preparedStatement.setString(5, Usuario.getEmail());
+            preparedStatement.setString(6, Usuario.getSenha());
+            preparedStatement.executeUpdate();
+
+            System.out.println(("O usuário foi gravado!!"));
+            connection.commit();
+        } catch (SQLException e) {
+            connection.rollback();
+           throw new SQLException("Erro ao cadastrar usuário", e);
+        } finally {
+            connection.close();
+        }
+
+    }
+    
+    @Override
+    public Usuario update(int id, Usuario Usuario) throws SQLException {
+        sql = "UPDATE T_USUARIO SET CD_USUARIO = ? , NM_USUARIO = ?, CD_GENERO = ?, DT_NASCIMENTO = ?, EMAIL = ?, SENHA = ? WHERE CD_USUARIO = ?";
+        try {
+            connection = FintechDB.getConnectionDB();
+            preparedStatement = connection.prepareStatement(
+                    sql
+            );
+
+            preparedStatement.setInt(1, Usuario.getId());
+            preparedStatement.setString(2, Usuario.getNomeUsuario());
+            Date date = Date.valueOf(Usuario.getDtNascimento());
+            preparedStatement.setDate(3, date);
+            preparedStatement.setInt(4, Usuario.getGenero().getId());
+            preparedStatement.setString(5, Usuario.getEmail());
+            preparedStatement.setString(6, Usuario.getSenha());
+            preparedStatement.executeUpdate();
+
+            System.out.println(("O usuário foi atualizada!!"));
+            connection.commit();
+        } catch (SQLException e) {
+            connection.rollback();
+            throw new SQLException("Erro ao editar usuário", e);
+        } finally {
+            connection.close();
+        }
+
+        return Usuario;
+
+    }
+    
+    @Override
+    public void delete(int id) throws SQLException {
+        sql = "DELETE T_USUARIO WHERE CD_USUARIO = ?";
+        try {
+            connection = FintechDB.getConnectionDB();
+            preparedStatement = connection.prepareStatement(
+                    sql
+            );
+
+            preparedStatement.setInt(1, id);
+            preparedStatement.executeUpdate();
+
+            System.out.println(("O usuário foi deletado!!"));
+            connection.commit();
+        } catch (SQLException e) {
+            connection.rollback();
+            throw new SQLException("Erro ao excluir usuário", e);
+        } finally {
+            connection.close();
+        }
+    }
+    
+    @Override
+    public List<Usuario> getAll() throws SQLException {
+        List<Usuario> listUsuario = new ArrayList<Usuario>();
+        sql = "SELECT * FROM T_USUARIO";
+        try {
+            connection = FintechDB.getConnectionDB();
+            statement  = connection.createStatement();
+
+            ResultSet result = statement.executeQuery(sql);
+
+            while (result.next()) {
+            	Genero genero = new Genero();
+            	genero.setId(result.getInt("cd_genero"));
+                Usuario usuario = new Usuario(result.getInt("cd_usuario"), result.getString("nm_usuario"),
+                		result.getDate("dt_nascimento").toLocalDate(), genero,
+                		result.getString("email"), result.getString("senha"));
+
+                listUsuario.add(usuario);
+            }
+
+            connection.commit();
+        } catch (SQLException e) {
+            connection.rollback();
+            throw new SQLException("Erro ao cadastrar usuário", e);
+        } finally {
+            connection.close();
+        }
+
+        return listUsuario;
+    }
+}
